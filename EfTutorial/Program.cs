cef362e07d15719ea13600cdb5e83a695fb9eecd
Greg Doud <gpdoud@gmail.com>
2021-02-26 16:01:48 -0500

    Finished Database First

--- EfTutorial/Program.cs
+++ EfTutorial/Program.cs
@@ -1,10 +1,78 @@
-﻿using System;
+﻿using EfTutorial.Models;
+
+using System;
 using System.Linq;
 
 namespace EfTutorial {
     class Program {
         static void Main(string[] args) {
 
+            var sctrl = new StudentsController();
+
+            //var sGreg = new Student {
+            //    Id = 0, Firstname = "Greg", Lastname = "Doud", StateCode = "OH",
+            //    Gpa = 2.1m, Sat = 805, MajorId = 1
+            //};
+            //var sGregNew = sctrl.Create(sGreg);
+            //Console.WriteLine($"{sGregNew.Id} {sGregNew.Firstname} {sGregNew.Lastname}");
+
+            var std = sctrl.GetByPK(63);
+
+            std.Firstname = "Gregory";
+            sctrl.Change(std);
+
+            var studentDeleted = sctrl.Remove(std.Id);
+
+
+            var st = sctrl.GetByPK(63);
+            if(st == null) {
+                Console.WriteLine("Not found");
+            } else {
+                Console.WriteLine($"{st.Firstname} {st.Lastname}");
+            }
+
+            st = sctrl.GetByPK(11111);
+            if(st == null) {
+                Console.WriteLine("Not found");
+            } else {
+                Console.WriteLine($"{st.Firstname} {st.Lastname}");
+            }
+
+            var students = sctrl.GetAll();
+            foreach(var s in students) {
+                Console.WriteLine($"{s.Id} {s.Firstname} {s.Lastname}");
+            }
+
+        }
+        static void Run1() { 
+
+            var _context = new eddbContext();
+            _context.Students.ToList()
+                .ForEach(s => Console.WriteLine($"{s.Firstname} {s.Lastname}"));
+
+            var majors = from m in _context.Majors
+                         where m.MinSat > 1000
+                         orderby m.Description
+                         select m;
+            foreach(var m in majors) {
+                Console.WriteLine($"{m.Description} | {m.MinSat}");
+            }
+
+            // Join Students & Majors. Print Name and Major
+
+            var allStudents = (from s in _context.Students
+                              join m in _context.Majors
+                              on s.MajorId equals m.Id into grp
+                              from mm in grp.DefaultIfEmpty()
+                              select new {
+                                  Name = s.Firstname + " " + s.Lastname,
+                                  Major = mm == null ? "Undeclared" : mm.Description
+                              }).ToList();
+            
+            
+            
+           
+            allStudents.ForEach(s => Console.WriteLine($"{s.Name} - {s.Major}"));
         }
     }
 }
---
+++ EfTutorial/StudentsController.cs
@@ -0,0 +1,85 @@
+﻿using EfTutorial.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EfTutorial {
+    
+    public class StudentsController {
+
+        private readonly eddbContext _context;
+
+        // read all the student with SAT score between 1000 and 1200 inclusive
+        // and order the result by SAT score descending
+        public IEnumerable<Student> GetBySatRange(int LowSat, int HighSat) {
+
+            return _context.Students
+                            .Where(s => s.Sat >= LowSat && s.Sat <= HighSat)
+                            .OrderByDescending(s => s.Sat)
+                            .ToList();
+
+            return (from s in _context.Students
+                   where s.Sat >= LowSat && s.Sat <= HighSat
+                   orderby s.Sat descending
+                   select s).ToList();
+        }
+
+
+        public IEnumerable<Student> GetAll() {
+            return _context.Students.ToList();
+        }
+
+        public Student GetByPK(int id) {
+            return _context.Students.Find(id);
+        }
+
+        public Student Create(Student student) {
+            if(student == null) {
+                throw new Exception("Student cannot be null!");
+            }
+            if(student.Id != 0) {
+                throw new Exception("Student.Id must be zero!");
+            }
+            _context.Students.Add(student);
+            var rowsAffected = _context.SaveChanges();
+            if(rowsAffected != 1) {
+                throw new Exception("Create failed!");
+            }
+            return student;
+        }
+
+        public void Change(Student student) {
+            if(student == null) {
+                throw new Exception("Student cannot be null!");
+            }
+            if(student.Id <= 0) {
+                throw new Exception("Student.Id must be greater than zero!");
+            }
+            _context.Entry(student).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            var rowsAffected = _context.SaveChanges();
+            if(rowsAffected != 1) {
+                throw new Exception("Change failed!");
+            }
+            return;
+        }
+
+        public Student Remove(int id) {
+            var student = _context.Students.Find(id);
+            if(student == null) {
+                return null;
+            }
+            _context.Students.Remove(student);
+            var rowsAffected = _context.SaveChanges();
+            if(rowsAffected != 1) {
+                throw new Exception("Remove failed!");
+            }
+            return student;
+        }
+
+        public StudentsController() {
+            _context = new eddbContext();
+        }
+    }
+}
